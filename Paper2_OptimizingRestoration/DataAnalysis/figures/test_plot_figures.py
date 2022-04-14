1be9059f649ecbbbef648861015f95dfddb7e085
bknorris <bnorris0600@gmail.com>
2022-04-13 17:25:39 -0700

    Added turbulence profile plot routine

--- Paper2_OptimizingRestoration/DataAnalysis/figures/test_plot_figures.py
+++ Paper2_OptimizingRestoration/DataAnalysis/figures/test_plot_figures.py
@@ -20,7 +20,7 @@ model_source = Path('c:/Users/bknorris/Documents/Models/Paper2_OptimizingRestora
 save_fig_dir = Path('c:/Users/bknorris/Documents/Models/Paper2_OptimizingRestoration/Figures')
 csv_file = 'modelPostProcessing.csv'
 data_file = 'modelPostProcessing_5s_10s_V1.dat'
-save_figures = True
+save_figures = False
 
 # Load binary results file
 file = open(model_source / data_file, 'rb')
@@ -182,6 +182,126 @@ if save_figures:
                 facecolor='auto', edgecolor='auto',
                 backend=None)
 
-# Eps_norm(z) vs Z (colored by TP)
+
+# Eps_norm(z) vs Z (colored by deltaS)
+plt.style.use('_mpl-gallery')
+f3 = plt.figure(figsize=(10, 6.5))
+axes1 = f3.add_axes([0.1, 0.1, 0.25, 0.8])
+axes2 = f3.add_axes([0.4, 0.1, 0.25, 0.8])
+axes3 = f3.add_axes([0.7, 0.1, 0.25, 0.8])
+ax = [axes1, axes2, axes3]
+
+# Colormaps from ColorBrewer
+BuGn = ['#ccece6', '#99d8c9', '#66c2a4', '#41ae76', '#238b45']
+OrRd = ['#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f']
+PuBu = ['#d0d1e6', '#a6bddb', '#74a9cf', '#3690c0', '#0570b0']
+cmap = [BuGn, OrRd, PuBu]
+
+# Marker spec
+markers = ['o', '^', 's', 'd', 'P']
+
+# Plot 5 s waves first
+scenario = model_info.orgScenarioNumber[five].tolist()
+scenario = [str(x) for x in scenario]
+waveHeight = model_info.waveHeight[five]
+wave_unq = np.unique(waveHeight)
+spacing = model_info.deltaS[five]
+spce_unq = np.unique(spacing)
+
+count = 1
+for i in range(0, len(wave_unq)):
+    for j in range(0, len(spce_unq)):
+        idx1 = np.where(waveHeight == wave_unq[i])[0].tolist()
+        idx2 = np.where(spacing == spce_unq[j])[0].tolist()
+        idx = np.intersect1d(idx1, idx2)[0]
+        
+        x = data['eps_norm'][scenario[idx]]
+        y = np.linspace(-0.028, -0.001, len(x)) * 36
+        
+        x_scaled = spce_unq[j] * 36
+        leg = f'$\Delta S = {x_scaled:.1f}$'
+        ax[i].plot(x, y, color=cmap[i][j],
+                 marker=markers[j],
+                 markersize=6,
+                 label=leg)
+    
+    count += 1
+    
+# Plot Adjustments
+axes1.set_ylim(-1.1, 0)
+axes1.set_xlim(-1e-4, 5e-3)
+axes1.set_xticks(np.linspace(1e-3, 5e-3, 3))
+axes1.legend(loc='upper right', frameon=True, framealpha=1)
+axes1.set_title(r'$T_w = \ \mathrm{0.05 m}$')
+axes1.set_ylabel('Depth (m)')
+axes1.set_xlabel(r'$\epsilon \left/ (g^3h)^{1/2} \right.$')
+
+axes2.set_ylim(-1.1, 0)
+axes2.set_xlim(-1e-4, 5e-3)
+axes2.set_xticks(np.linspace(1e-3, 5e-3, 3))
+axes2.legend(loc='upper right')
+axes2.set_title(r'$T_w = \ \mathrm{0.15 m}$')
+axes2.yaxis.set_ticklabels([])
+axes2.set_xlabel(r'$\epsilon \left/ (g^3h)^{1/2} \right.$')
+
+axes3.set_ylim(-1.1, 0)
+axes3.set_xlim(-1e-4, 5e-3)
+axes3.set_xticks(np.linspace(1e-3, 5e-3, 3))
+axes3.legend(loc='upper right')
+axes3.set_title(r'$T_w = \ \mathrm{0.30 m}$')
+axes3.yaxis.set_ticklabels([])
+axes3.set_xlabel(r'$\epsilon \left/ (g^3h)^{1/2} \right.$')
+
+# Save figure
+if save_figures:
+    fname = 'Eps_norm_vs_z_5s.png'
+    plt.savefig(save_fig_dir / fname, dpi=300, format=None, metadata=None,
+                bbox_inches=None, pad_inches=0.1,
+                facecolor='auto', edgecolor='auto',
+                backend=None)
+
+
 # max(Eps_norm) vs Tp
+plt.style.use('_mpl-gallery')
+f4 = plt.figure(figsize=(5, 4))
+axes = f4.add_axes([0.175, 0.12, 0.8, 0.8])
+
+# Colormaps from ColorBrewer
+BuGn = ['#ccece6', '#99d8c9', '#66c2a4', '#41ae76', '#238b45']
+OrRd = ['#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f']
+PuBu = ['#d0d1e6', '#a6bddb', '#74a9cf', '#3690c0', '#0570b0']
+cmap = [BuGn, OrRd, PuBu]
+
+# Marker spec
+markers = ['o', '^', 's', 'd', 'P']
+
+# Plot 5 s waves first
+scenario = model_info.orgScenarioNumber[five].tolist()
+scenario = [str(x) for x in scenario]
+waveHeight = model_info.waveHeight[five]
+wave_unq = np.unique(waveHeight)
+wavePeriod = model_info.wavePeriod[five]
+spacing = model_info.deltaS[five]
+spce_unq = np.unique(spacing)
+
+count = 1
+for i in range(0, len(wave_unq)):
+    for j in range(0, len(spce_unq)):
+        idx1 = np.where(waveHeight == wave_unq[i])[0].tolist()
+        idx2 = np.where(spacing == spce_unq[j])[0].tolist()
+        idx = np.intersect1d(idx1, idx2)[0]
+        
+        y = np.max(data['eps_norm'][scenario[idx]])
+        x = wavePeriod.tolist()[idx]
+        
+        x_scaled = spce_unq[j] * 36
+        leg = f'$\Delta S = {x_scaled:.1f}$'
+        plt.plot(x, y, color=cmap[i][j],
+                 marker=markers[j],
+                 markersize=6,
+                 label=leg)
+    
+    count += 1
+
+
 # Runup at shore vs Tp
